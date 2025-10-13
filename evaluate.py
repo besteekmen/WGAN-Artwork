0e3af892e1fa00d26149f073ecf4af4808d822d1
Beste Ekmen <besteekmen@gmail.com>
2025-10-13 11:36:09 +0300

    v14 model: test added and bugfix done for torch.inference call

--- evaluate.py
+++ evaluate.py
@@ -1,21 +1,14 @@
 import os
-
-from debugpy.adapter.components import missing
-from numba.core.cgutils import printf
-from sympy import composite
 from tqdm import tqdm
 
 import torch
-import torch.nn.functional as F
-from torch.utils.data import DataLoader
 from torchvision import transforms
 import torchvision.utils as vutils
-from torchvision.utils import save_image
 
 from models.aot_generator import AOTGenerator
 from dataset import CroppedImageDataset, randomize_masks, make_dataloader
-from utils.utils import to_unit, to_signed, print_device, set_seed, get_device, is_cuda, to_u8
-from config import CROP_SIZE, NUM_WORKERS, SEED, BATCH_SIZE, DATA_PATH
+from utils.utils import to_unit, print_device, set_seed, get_device, is_cuda, to_u8
+from config import NUM_WORKERS, SEED, BATCH_SIZE, DATA_PATH
 
 from torchmetrics.image.ssim import StructuralSimilarityIndexMeasure
 from torchmetrics.image.fid import FrechetInceptionDistance
@@ -43,7 +36,7 @@ def evaluate(model_path, out_dir="eval_outputs",
     device = get_device()
 
     # Load generator
-    netG = AOTGenerator().to(device).eval()
+    netG = AOTGenerator(in_channels=4).to(device).eval()
     load_state(netG, model_path, device)
 
     # Load validation dataset
@@ -59,6 +52,7 @@ def evaluate(model_path, out_dir="eval_outputs",
     ssim = StructuralSimilarityIndexMeasure(data_range=1.0).to(device)
     lpips = LearnedPerceptualImagePatchSimilarity(net_type='vgg').to(device)
     fid = FrechetInceptionDistance(normalize=True).to(device)
+    fid.reset()
 
     if save_images:
         os.makedirs(out_dir, exist_ok=True)
@@ -67,38 +61,39 @@ def evaluate(model_path, out_dir="eval_outputs",
     counter = 0
 
     # Evaluation loop
-    for image, mask in tqdm(test_loader, desc="Testing", ncols=100):
-        image = image.to(device, non_blocking=True)
-        mask_known = randomize_masks(mask.to(device), irr_ratio=irr_ratio)
-        mask_hole = (1.0 - mask_known).float()
+    with torch.inference_mode():
+        for image, mask in tqdm(test_loader, desc="Testing", ncols=100):
+            image = image.to(device, non_blocking=True)
+            mask_known = randomize_masks(mask.to(device), irr_ratio=irr_ratio)
+            mask_hole = (1.0 - mask_known).float()
 
-        fake = netG(image, mask_hole)
-        masked = image * mask_known
-        comp = masked + fake * mask_hole
+            fake = netG(image, mask_hole)
+            masked = image * mask_known
+            comp = masked + fake * mask_hole
 
-        unit_image = to_unit(image) # [0,1]
-        unit_comp = to_unit(comp) # [0,1]
+            unit_image = to_unit(image) # [0,1]
+            unit_comp = to_unit(comp) # [0,1]
 
-        ssim_batch = ssim(unit_comp, unit_image).item()
-        ssim_tot += ssim_batch
+            ssim_batch = ssim(unit_comp, unit_image).item()
+            ssim_tot += ssim_batch
 
-        lpips_batch = lpips(unit_image, unit_comp).item()
-        lpips_tot += lpips_batch
+            lpips_batch = lpips(unit_image, unit_comp).item()
+            lpips_tot += lpips_batch
 
-        fid.update(to_u8(unit_image), real=True)
-        fid.update(to_u8(unit_comp), real=False)
+            fid.update(to_u8(unit_image), real=True)
+            fid.update(to_u8(unit_comp), real=False)
 
-        if save_images:
-            grid = torch.cat(  # dimensions: (C,H,W)
-                [unit_image, to_unit(masked), unit_comp], 3)
-            vutils.save_image(
-                grid,
-                os.path.join(out_dir, f'test_image({counter}).jpg'),
-                normalize=False,
-                nrow=1
-            )
-            counter += 1
-        batch += 1
+            if save_images:
+                grid = torch.cat(  # dimensions: (C,H,W)
+                    [unit_image, to_unit(masked), unit_comp], 3)
+                vutils.save_image(
+                    grid,
+                    os.path.join(out_dir, f'test_image({counter}).jpg'),
+                    normalize=False,
+                    nrow=1
+                )
+                counter += 1
+            batch += 1
 
     ssim_avg = ssim_tot / max(1, batch)
     lpips_avg = lpips_tot / max(1, batch)
